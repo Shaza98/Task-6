13d6d8895ae99c74ba24f296ae1f136e27fabc7f
Shaza98 <mansourshaza@yahoo.com>
2018-12-03 01:13:06 +0200

    Initial commit

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+import csv
+with open('file.csv', 'w') as f:
+    w = csv.writer(f, quoting=csv.QUOTE_ALL) 
+    w.writerow(["name", "email", "mobile", "university", "major"])
+
+    while (1):
+        name = input()
+        email = input()
+        mobile = int(input())
+        university = input()
+        major = input()
+        w.writerow([name, email, mobile, university, major])
+        if(name or email or mobile or university or major == "STOP"): break
