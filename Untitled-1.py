b9dd20e6916beba51dd97a8e9915599ff006e240
Shaza98 <mansourshaza@yahoo.com>
2018-12-03 01:29:13 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,9 +5,13 @@ with open('file.csv', 'w') as f:
 
     while (1):
         name = input()
+        if(name== "STOP"): break
         email = input()
+        if(email== "STOP"): break
         mobile = int(input())
+        if(mobile== "STOP"): break
         university = input()
+        if(university== "STOP"): break
         major = input()
-        w.writerow([name, email, mobile, university, major])
-        if(name or email or mobile or university or major == "STOP"): break
+        if(major == "STOP"): break
+        w.writerow([name, email, mobile, university, major, "\n"])
