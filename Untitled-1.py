d0fe15f15f82a323f8504647cbc0fc55671d53c5
Shaza98 <mansourshaza@yahoo.com>
2018-12-04 21:49:36 +0200

    edit

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,17 +1,27 @@
 import csv
+data = []
 with open('file.csv', 'w') as f:
     w = csv.writer(f, quoting=csv.QUOTE_ALL) 
-    w.writerow(["name", "email", "mobile", "university", "major"])
-
-    while (1):
-        name = input()
-        if(name== "STOP"): break
-        email = input()
-        if(email== "STOP"): break
-        mobile = int(input())
-        if(mobile== "STOP"): break
-        university = input()
-        if(university== "STOP"): break
-        major = input()
-        if(major == "STOP"): break
-        w.writerow([name, email, mobile, university, major, "\n"])
+    w.writerow(["Name", "Email", "Mobile", "University", "Major"])
+    
+while True:
+    name = input()
+    if(name== "STOP"): break 
+    data.append(name)   
+    email = input()
+    if(email== "STOP"): break
+    data.append(email)
+    mobile = int(input())
+    if(mobile== "STOP"): break
+    data.append(mobile)
+    university = input()
+    if(university== "STOP"): break
+    data.append(university)
+    major = input()
+    if(major == "STOP"): break  
+    data.append(major)  
+  
+i=0
+while True:
+    w.writerow([data(i), data(i+1), data(i+2), data(i+3), data(i+4)])
+    i+=1
